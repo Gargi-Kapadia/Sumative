e0b1c958cb2ae06821c3240b3cfe52c6571dd1d0
Kapadia <Gargi.Kapadia@student.tdsb.on.ca>
2023-01-17 11:08:43 -0500

    font awesome imports working

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import './style.css'
 import router from "./router/index.js"
 import { createPinia } from 'pinia';
+import { library } from '@fortawesome/fontawesome-svg-core' 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons' 
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons' 
+
+library.add(faUserSecret)
+library.add( faCartShopping)
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).use(pinia).mount('#app')
